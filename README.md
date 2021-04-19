6c083c472d7a574edddb58e292db76312ddadbad
M Alfa Mody Aulya <m.alfa.mody@batmandiri.com>
2021-04-19 14:03:48 +0700

    init

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # restspring
+
+Learning material for making Rest API with springboot
+
+based on
+
+https://erthru.medium.com/membangun-restapi-menggunakan-framework-spring-boot-7b22e25d3bd0
+
+https://github.com/erthru/Spring-PJ
---
+++ src/test/java/com/alfacool/restspring/RestspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alfacool.restspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
